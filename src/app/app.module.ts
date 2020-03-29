79bf95b67c7eaef52e15ef9164e39e71bf9a268e
XAVIERBF98 <jbaronfontanilla@gmail.com>
2020-03-29 11:00:51 -0500

    Modificacion en los Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTES } from './app.routes';
+
+//Modules
+import { PageModule } from './pages/pages.modules';
+
+//cOMPONENTeS
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LiginComponent } from './ligin/ligin.component';
+import { RegisterComponent } from './ligin/register.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTES,
+    PageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
